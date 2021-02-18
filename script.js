f31dfa67e6c57aba8bb6d825571c4e4234ae7ea7
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-02-17 23:36:53 -0600

    added html and css for the side panel

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // set random color for background of shapes div
+setBgColor = document.getElementById("hold-shapes");
 function randomBgColor() {
     let rgbColorOne = Math.floor(Math.random() * 256);
     let rgbColorTwo = Math.floor(Math.random() * 256);
     let rgbColorThree = Math.floor(Math.random() * 256);
     let bgColor = `rgb(${rgbColorOne}, ${rgbColorTwo}, ${rgbColorThree})`;
-    document.body.style.background = bgColor;
+    setBgColor.style.background = bgColor;
     }
 
     randomBgColor();
